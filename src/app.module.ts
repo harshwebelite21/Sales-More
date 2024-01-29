a8d3ace09a55d3723d783ee626020ce351472fc5
Harsh <harshvaghasiya@webelight.co.in>
2024-01-29 10:08:02 +0530

    Revert "Cart functionality"
    This reverts commit 83ea497b82a1cb70635ab3e1a4a2ef908b733dcc.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CartModule } from './cart/cart.module';
-import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database/database.module';
-import { UserModule } from './user/user.module';
-import { OrderModule } from './order/order.module';
-import { ProductModule } from './products/products.module';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule, CartModule,UserModule,OrderModule,ProductModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
- 
-}
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
-import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 async function bootstrap(): Promise<void> {
-  dotenv.config(); // Load environment variables from .env file
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
