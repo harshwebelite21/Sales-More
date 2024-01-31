cf09e2dd69abb40fd306a47a200ec0d219142b94
Harsh <harshvaghasiya@webelight.co.in>
2024-01-31 15:14:57 +0530

    Order model Created

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { DatabaseModule } from './modules/database/database.module';
 import { ProductModule } from './modules/products/products.module';
 import { AuthGuard } from './auth_guard/authGuard';
 import { CartModule } from './modules/cart/cart.module';
+import { OrderModule } from './modules/order/order.module';
 
 @Module({
-  imports: [UserModule, DatabaseModule, ProductModule, CartModule],
+  imports: [UserModule, DatabaseModule, ProductModule, CartModule, OrderModule],
   controllers: [AppController],
   providers: [AppService, AuthGuard],
 })
---
+++ src/modules/order/order.model.ts
@@ -0,0 +1,38 @@
+import { Schema } from 'mongoose';
+
+export const OrderSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    products: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          ref: 'Product',
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+    amount: {
+      type: Number,
+      require: true,
+    },
+  },
+  {
+    timestamps: true, // Add createdAt and updatedAt fields
+  },
+);
+
+export interface Order {
+  userId: string;
+  products: Array<{
+    productId: string;
+    quantity: number;
+  }>;
+  amount: number;
+}
