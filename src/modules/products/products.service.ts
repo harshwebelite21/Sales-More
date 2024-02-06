bd5957492686a1698cf459db551229e30e1581b8
Harsh <harshvaghasiya@webelight.co.in>
2024-02-06 17:47:20 +0530

    ready to compare

--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Product } from './products.model';
-import { AddProductDto, UpdateProductDto } from './dto/product.dto';
+import { Model, PipelineStage } from 'mongoose';
+import { Product, SortEnum } from './products.model';
+import {
+  AddProductDto,
+  FilterProductDto,
+  UpdateProductDto,
+} from './dto/product.dto';
 
 @Injectable()
 export class ProductService {
@@ -39,5 +43,50 @@ export class ProductService {
   async deleteProduct(productId: string): Promise<boolean> {
     await this.productModel.findByIdAndDelete(productId);
     return true;
+  }
+
+  // Filter Product
+  async filterProduct(queryData: FilterProductDto) {
+    const {
+      name,
+      category,
+      maxPrice,
+      minPrice,
+      attributeName,
+      attributeValue,
+      pageNumber = 1,
+      pageSize = 10,
+      sortBy = 'createdAt', // New parameter for sorting
+      sortOrder = SortEnum.DESC, // Default to ascending order
+    } = queryData;
+    const regex = new RegExp(name, 'i');
+
+    const sortStage: PipelineStage = {
+      $sort: {
+        [sortBy]: sortOrder === SortEnum.DESC ? 1 : -1,
+      },
+    };
+    const query = {
+      ...(name && { name: { $regex: regex } }),
+      ...(category && { category }),
+      ...(attributeName && { 'attributes.name': attributeName }),
+      ...(attributeValue && { 'attributes.value': attributeValue }),
+      ...(minPrice &&
+        maxPrice && { price: { $gte: minPrice, $lte: maxPrice } }),
+    };
+
+    const pipeline: PipelineStage[] = [
+      {
+        $match: query,
+      },
+      sortStage,
+      {
+        $skip: (pageNumber - 1) * pageSize, // Skip documents based on the page number
+      },
+      {
+        $limit: pageSize, // Limit the number of documents per page
+      },
+    ];
+    return this.productModel.aggregate(pipeline).exec();
   }
 }
