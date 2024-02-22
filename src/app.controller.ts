b51b02f947acf2198287857057a3fde9de906a7a
Harsh Vaghasiya <harshvaghasiya@webelight.co.in>
2024-02-22 13:15:32 +0530

    Home Page Updated

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
@@ -8,7 +9,9 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Res() res: Response): void {
+    const htmlContent = this.appService.getHello();
+    res.setHeader('Content-Type', 'text/html');
+    res.send(htmlContent);
   }
 }
